21b697cdc3e51eb521cdc3fcb97cbed771ba687b
Paulo Augusto Ponciano <paponciano@gmail.com>
2021-12-01 11:52:14 -0300

    Entrada dinâmica de data pelo usuário

--- Program.cs
+++ Program.cs
@@ -12,7 +12,13 @@ namespace ProgramacaoOrientadaObjetos
             objetoPessoa.Sobrenome = "Ponciano";
             Console.WriteLine($"O nome da pessoa é: {objetoPessoa.Nome} {objetoPessoa.Sobrenome}");
 
-            Pessoa novoObjetoPessoa = new("Maria", "Silva", new DateTime(1989, 11, 22));
+            Console.WriteLine("Digite a sua data de nascimento: (dd/MM/aaaa)");
+            string? textoDataNascimento = Console.ReadLine();
+            int ano = Convert.ToInt32(textoDataNascimento?.Substring(6, 4));
+            int mes = Convert.ToInt32(textoDataNascimento?.Substring(3, 2));
+            int dia = Convert.ToInt32(textoDataNascimento?.Substring(0, 2));
+            
+            Pessoa novoObjetoPessoa = new("Maria", "Silva", new DateTime(ano, mes, dia));
             Console.WriteLine($"O nome da pessoa do novo objeto é: {novoObjetoPessoa.Nome} {novoObjetoPessoa.Sobrenome}. {novoObjetoPessoa.CalculaIdade()}");   
         }
     }
