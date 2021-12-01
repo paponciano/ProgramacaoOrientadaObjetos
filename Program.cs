bd2e5ca5a2b532024c31b15ee22865c4819cc7c9
Paulo Augusto Ponciano <paponciano@gmail.com>
2021-12-01 09:13:05 -0300

    Commit inicial do projeto Programação Orientada a Objetos

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ProgramacaoOrientadaObjetos.Cadastros;
+
+namespace ProgramacaoOrientadaObjetos
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pessoa objetoPessoa = new();
+            objetoPessoa.Nome = "Paulo";
+            objetoPessoa.Sobrenome = "Ponciano";
+            Console.WriteLine($"O nome da pessoa é: {objetoPessoa.Nome} {objetoPessoa.Sobrenome}");
+
+            Pessoa novoObjetoPessoa = new("Maria", "Silva", DateTime.Now);
+            Console.WriteLine($"O nome da pessoa do novo objeto é: {novoObjetoPessoa.Nome} {novoObjetoPessoa.Sobrenome}");
+
+        }
+    }
+}
